c23e8e7680930c6c36a9a4bf024662fd882ec56a
Jonathan Panuelos <jonathan.panuelos@gmail.com>
2025-11-17 18:56:31 -0500

    Update pressure_projection.py

--- assignment/pressure_projection.py
+++ assignment/pressure_projection.py
@@ -49,5 +49,4 @@ def pressure_projection(
         Velocity is set to 0 at faces adjacent to solid cells.
     """
     grid_x, grid_y, grid_z = wp.tid()
-    state.grid_v_out[grid_x, grid_y, grid_z] = state.grid_v_in[grid_x, grid_y, grid_z]
 
